65b865936592e3e210e3f8f3136099c1140495e4
Daniel Rodríguez Fernández <daniel.rodriguezfernandez@alumno.iesluisvives.org>
2022-10-03 10:21:09 +0200

    Enums Modificados

--- AD-P1-Proyecto/src/main/kotlin/models/ContenedoresVarios.kt
+++ AD-P1-Proyecto/src/main/kotlin/models/ContenedoresVarios.kt
@@ -1,24 +1,27 @@
 package models
 
+import enums.TipoContenedor
 import java.io.File
 
 data class ContenedoresVarios(
-    val codigoInternoSituado: String,
-    val tipoContenedor: String,
-    val modelo: String,
-    val descripcionModelo: String,
-    val cantidad: String,
-    val lote: String,
-    val distrito: String,
-    val barrio: String,
-    val tipoVia: String,
-    val nombre: String,
-    val numero: String,
-    val coordenadaX: String,
-    val coordenadaY: String,
-    val TAG: String
+    val codigoInternoSituado: String?,
+    val tipoContenedor: TipoContenedor?,
+    val modelo: String?,
+    val descripcionModelo: String?,
+    val cantidad: String?,
+    val lote: String?,
+    val distrito: String?,
+    val barrio: String?,
+    val tipoVia: String?,
+    val nombre: String?,
+    val numero: String?,
+    val coordenadaX: String?,
+    val coordenadaY: String?,
+    val TAG: String?
 
-)
+) {
+
+}
 
 fun loadContenedoresVariosCSV(csvFile: File): List<ContenedoresVarios>{
     val contenedoresVarios: List<ContenedoresVarios> = csvFile.readLines()
@@ -28,22 +31,33 @@ fun loadContenedoresVariosCSV(csvFile: File): List<ContenedoresVarios>{
             it.map { campo -> campo.trim() }
             ContenedoresVarios(
                 codigoInternoSituado = it[0],
-                tipoContenedor = it[2],
-                modelo = it[3],
-                descripcionModelo = it[4],
-                cantidad = it[5],
-                lote = it[6],
-                distrito = it[7],
-                barrio = it[8],
-                tipoVia = it[9],
-                nombre = it[10],
-                numero = it[11],
-                coordenadaX = it[12],
-                coordenadaY = it[13],
-                TAG = it[14]
+                tipoContenedor = getEnumTipoContenedor(it[1]),
+                modelo = it[2],
+                descripcionModelo = it[3],
+                cantidad = it[4],
+                lote = it[5],
+                distrito = it[6],
+                barrio = it[7],
+                tipoVia = it[8],
+                nombre = it[9],
+                numero = it[10],
+                coordenadaX = it[11],
+                coordenadaY = it[12],
+                TAG = it[13]
             )
         }
     return contenedoresVarios
+}
+
+fun getEnumTipoContenedor(s: String): TipoContenedor {
+    when (s) {
+        "Envases" -> return TipoContenedor.ENVASES
+        "Organica" ->return TipoContenedor.ORGANICA
+        "Resto" -> return TipoContenedor.RESTO
+        "Papel y carton" -> return TipoContenedor.PAPEL_Y_CARTÓN
+        "Vidrio" -> return TipoContenedor.VIDRIO
+    }
+    return TipoContenedor.DESCONOCIDO
 }
 
 
