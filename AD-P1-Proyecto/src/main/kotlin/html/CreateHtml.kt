12bf6105aae4a2695358a8148ab9eaced6b0f932
azahara <blancaluna92@hotmail.com>
2022-10-16 14:49:53 +0200

    creado html districy con datos, falta las gráficas

--- AD-P1-Proyecto/src/main/kotlin/html/CreateHtml.kt
+++ AD-P1-Proyecto/src/main/kotlin/html/CreateHtml.kt
@@ -2,6 +2,8 @@ package html
 
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.forEach
+import org.jetbrains.kotlinx.dataframe.io.html
+import org.jetbrains.kotlinx.dataframe.io.toHTML
 
 class CreateHtml {
 
@@ -31,6 +33,7 @@ class CreateHtml {
                            graficoDemaxMinMedYDes: Any?,
                            contenedoresPorTipo: DataFrame<Any?>?): String {
 
+
         var html : String = ("""
             <!DOCTYPE html>
             <html lang="es">
@@ -56,32 +59,22 @@ class CreateHtml {
                             <h2>Contenidos</h2>
                         </li>
                         <li> <br>
-                            <a href=""style="color: #355835 ;" >Número de contenedores de cada tipo que hay en cada distrito.</a> <br>
-                            ${contenedoresPorTipo?.forEach { x -> "<p> $x </p>" }} 
+                            <a href=""style="color: #355835 ;" >1. Número de contenedores de cada tipo que hay en este distrito.</a> <br>
+                           
                         </li>
                         <li> <br>
-                            <a href="" style="color: #355835 ;">Media de contenedores de cada tipo que hay en cada distrito.</a> <br>
+                            <a href="" style="color: #355835 ;">2. Total de toneladas recogidas en ese distrito por residuo.</a> <br>
                         </li>
                         <li> <br>
-                            <a href=""style="color: #355835 ;">Gráfico con el total de contenedores por distrito.</a>
+                            <a href=""style="color: #355835 ;">3.Gráfico con el total de toneladas por residuo en ese distrito.</a>
                         </li>
                         <li> <br>
-                            <a href=""style="color: #355835 ;">Media de toneladas anuales de recogidas por cada tipo de basura agrupadas por
-                                distrito.</a>
+                            <a href=""style="color: #355835 ;">4.Máximo, mínimo , media y desviación por mes por residuo en dicho distrito.</a>
                         </li>
                         <li> <br>
-                            <a href=""style="color: #355835 ;"> Gráfico de media de toneladas mensuales de recogida de basura por distrito.</a>
+                            <a href=""style="color: #355835 ;">5.Gráfica del máximo, mínimo y media por meses en dicho distrito.</a>
                         </li>
-                        <li> <br>
-                            <a href=""style="color: #355835 ;">Máximo, mínimo , media y desviación de toneladas anuales de recogidas por cada tipo
-                                de basura agrupadas por distrito.</a>
-                        </li>
-                        <li> <br>
-                            <a href=""style="color: #355835 ;">Suma de todo lo recogido en un año por distrito.</a>
-                        </li>
-                        <li> <br>
-                            <a href=""style="color: #355835 ;">Por cada distrito obtener para cada tipo de residuo la cantidad recogida.</a>
-                        </li>
+                        
 
                     </ul>
                     <br>
@@ -91,33 +84,39 @@ class CreateHtml {
 
                     <ul>
                         <li>
-                            <h2>Número de contenedores de cada tipo que hay en cada distrito.</h2>
+                            <h2>1. Número de contenedores de cada tipo que hay en este distrito.</h2>
+                             <br>
+                             ${contenedoresPorTipo?.html()}
 
                         </li>
                         <li>
-                            <h2>Media de contenedores de cada tipo que hay en cada distrito.</h2>
+                            <h2>2. Total de toneladas recogidas en ese distrito por residuo.</h2>
+                            <br>
+                             ${toneladasPorResiduo?.html()} 
+                            
                         </li>
                         <li>
-                            <h2>Gráfico con el total de contenedores por distrito.</h2>
+                            <h2>3.Gráfico con el total de toneladas por residuo en ese distrito.</h2>
+                             <br>
+                             faltaGrafico
+                             
+                     
                         </li>
                         <li>
-                            <h2>Media de toneladas anuales de recogidas por cada tipo de basura agrupadas por
-                                distrito.</h2>
+                            <h2>4.Máximo, mínimo , media y desviación por mes por residuo en dicho distrito.</h2>
+                            <br>
+                            ${estadisticasPorResiduoMax?.toHTML()}
+                           ${estadisticasPorResiduoMin?.toHTML()}
+                           ${estadisticasPorResiduoMed?.toHTML()}
+                           ${estadisticasPorResiduoDesv?.toHTML()}
+                            
+                            
                         </li>
                         <li>
-                            <h2>Gráfico de media de toneladas mensuales de recogida de basura por distrito.</h2>
+                            <h2>5.Gráfica del máximo, mínimo y media por meses en dicho distrito.</h2>
+                             faltaGrafico
                         </li>
-                        <li>
-                            <h2>Máximo, mínimo , media y desviación de toneladas anuales de recogidas por cada tipo
-                                de basura agrupadas por distrito.</h2>
-                        </li>
-                        <li>
-                            <h2>Suma de todo lo recogido en un año por distrito.</h2>
-                        </li>
-                        <li>
-                            <h2>Por cada distrito obtener para cada tipo de residuo la cantidad recogida.</h2>
-                        </li>
-
+                        
                     </ul>
 
                 </div>
