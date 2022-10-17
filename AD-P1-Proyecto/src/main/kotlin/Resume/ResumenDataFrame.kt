62b14ece03a916c24566a764962fdca4e386325e
azahara <blancaluna92@hotmail.com>
2022-10-17 17:59:35 +0200

    casteado los dataframe a el objeto en resumen

--- AD-P1-Proyecto/src/main/kotlin/Resume/ResumenDataFrame.kt
+++ AD-P1-Proyecto/src/main/kotlin/Resume/ResumenDataFrame.kt
@@ -4,8 +4,11 @@ package Resume
 import DataframeUtils.Consultas
 import DataframeUtils.GetDataFrame
 import DataframeUtils.Graficos
+import dto.ModeloResiduoDTO
 import html.CreateHtml
 import logger
+import models.ContenedoresVarios
+import models.ModeloResiduo
 import org.jetbrains.kotlinx.dataframe.DataFrame
 import org.jetbrains.kotlinx.dataframe.api.*
 import java.nio.file.Path
@@ -158,10 +161,12 @@ class ResumenDataFrame {
 
                 //obtenemos los dataframe correspondientes con la clase GetDataFrame
                 logger.info("cojemos datos e mr")
-                var filasMr: DataFrame<Any?>? = GetDataFrame().dataFrameModeloResiduoTotal(pathMR)
+                var filasMrsinCast: DataFrame<Any?>? = GetDataFrame().dataFrameModeloResiduoTotal(pathMR)
+                var filasMr : DataFrame<Any?>? = filasMrsinCast?.cast<ModeloResiduo>()
 
                 logger.info("cojemos datos e cv")
-                var filasCv: DataFrame<Any?>? = GetDataFrame().dataframeContenedoresVariosTotal(pathCV)
+                var filasCvsinCast: DataFrame<Any?>? = GetDataFrame().dataFrameModeloResiduoTotal(pathCV)
+                var filasCv: DataFrame<Any?>? = filasCvsinCast?.cast<ContenedoresVarios>()
 
 
                 var numeroContenedoresPorDistrito : DataFrame<Any?>? = null
